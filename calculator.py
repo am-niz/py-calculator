57ba6a676d5d6deacd75867afcd750a3b99f5211
alen46 <alenjose48@gmail.com>
2023-12-10 11:45:57 +0530

    Added feature subtraction

--- calculator.py
+++ calculator.py
@@ -8,3 +8,8 @@ def mul():
     y=int(input("Enter the 2nd number: "))
     m=x*y
     print(f"The multiplication is {m}")
+def sub():
+    x=int(input("Enter the 1st number: "))
+    y=int(input("Enter the 2nd number: "))
+    diff=x-y
+    print(f"The difference is {diff}")
