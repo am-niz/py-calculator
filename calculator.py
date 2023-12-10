fbcb7d979c117e1466c039e124424b75f6c6d6e3
Amjad <amjadkj26523@gmail.com>
2023-12-10 11:02:40 +0530

    division function name changed

--- calculator.py
+++ calculator.py
@@ -8,7 +8,7 @@ def mul():
     y=int(input("Enter the 2nd number: "))
     m=x*y
     print(f"The multiplication is {m}")
-def div():
+def division():
     x=int(input("Enter the 1st number: "))
     y=int(input("Enter the 2nd number: "))
     d=x/y
